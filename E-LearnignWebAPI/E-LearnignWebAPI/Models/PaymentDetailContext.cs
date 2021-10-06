8b4bfbb90e459c23dff897f6b07f01193863168a
Nguyễn Đăng Thắng <56484113+unikey1919@users.noreply.github.com>
2021-10-06 20:03:22 +0700

    initial

---
+++ E-LearnignWebAPI/E-LearnignWebAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_LearnignWebAPI.Models
+{
+    public class PaymentDetailContext:DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
+    }
+}
