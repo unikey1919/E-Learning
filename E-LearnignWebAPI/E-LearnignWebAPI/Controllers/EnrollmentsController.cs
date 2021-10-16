54bfdc2890a3294fda4f5a7e72f955306d5040b2
Trịnh Duy Luân <duyluanqwe@gmail.com>
2021-10-16 21:02:42 +0700

    a

--- E-LearnignWebAPI/E-LearnignWebAPI/Controllers/EnrollmentsController.cs
+++ E-LearnignWebAPI/E-LearnignWebAPI/Controllers/EnrollmentsController.cs
@@ -23,7 +23,7 @@ namespace E_LearnignWebAPI.Controllers
 
         // GET: api/Enrollments
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Enrollment>>> GetEnrollments()
+        public async Task<ActionResult<IEnumerable<Enrollment>>> GetEnrollments() 
         {
             return await _context.Enrollments.ToListAsync();
         }
